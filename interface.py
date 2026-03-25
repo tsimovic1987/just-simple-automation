67b27359c38c10d12fa60d153423285e5504d4c1
Tomislav Simovic <tomislav.simovic@yahoo.com>
2026-03-25 13:17:24 +0100

    new base models class

--- interface.py
+++ interface.py
@@ -5,23 +5,13 @@ from models import Models
 class Interface(ctk.CTk):
     def __init__(self):
         super().__init__()
-        
-        # interface settings
         self.title("Test: Meine Modelle Klasse in ctk einbinden")
         self.geometry("400x400")
 
         self.scale = Models("Rollstuhlwaage", "Haushalt")
-
-        # just to fill the empty CTk window with some labelss and a button
 
         self.label_model = ctk.CTkLabel(self, text=f"Modell: {self.scale.model}")
         self.label_model.pack(pady=10)
 
         self.label_category = ctk.CTkLabel(self, text=f"Kategorie: {self.scale.category}")
         self.label_category.pack(pady=10)
-        
-        self.button = ctk.CTkButton(self, text="Info zeigen", command=self.show_details)
-        self.button.pack(pady=10)
-
-    def show_details(self):
-        print(self.scale.get_info())
--- models.py
+++ models.py
@@ -1,25 +1,33 @@
+
+# BASE CLASS
+
 class Models:
     def __init__(self, model: str, category: str, max_weight_g: int=50000, accuracy: int=10): 
         """
-        Initialize a new scale. By default, we assume a maximum weight of 50kg and increments
-        of 10g. It is best to always store weights internally in the smallest unit (grams)
-        to avoid rounding errors.
+        This class contains everything that EVERY electronic scale in the world has in common.
 
         More coming soon!
         """
+
+        # FIXED ATTRIBUTES (Base Model)
 
         self.model = model                  # Scale model
         self.category = category            # Scale mategory (industry, household, etc.)
         self.max_weight_g = max_weight_g    # The limit until ERR
         self.accuracy = accuracy            # coming soon!
+        # ...
+
+        # FLEXIBLE ATTRIBUTES (Custom Model)
+
+        self.is_on: bool = False
+        self.current_weight_0: int = 0
+        self.tare_weight_g: int = 0
+        self.is_overloaded: bool = False
+        # ...
+
+    
+    def __str__(self) -> str:
+        return f""
 
 
-
-
-
-    def __str__(self):
-        return f"Model: {self.model} - Category: {self.category}"
-
-
-    def get_info(self):
-        return self.__str__()
+    # ...
