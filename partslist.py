d5cc44b52eca0387b15762f8397e86cf2bdf0b6c
Tomislav Simovic <tomislav.simovic@yahoo.com>
2026-03-25 16:07:50 +0100

    delete the import line & delete the type annotation

--- partslist.py
+++ partslist.py
@@ -1,12 +1,11 @@
-from models import Models, PostOfficeScale
-
 class PartsList:
-    def __init__(self, model_obj: Models):
+    def __init__(self, model_obj):
         
         self.model = model_obj
+
+
         self.parts: dict = {
             f"Load cell ({self.model.max_weight_g  / 1000} kg)": 1,     # example load cell
             "Sechskantschraube M5": 5,                                  # example bolts
             "Rubber": 2                                                 # example rubber
         }
-        
