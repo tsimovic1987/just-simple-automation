b931335cdb1a8863ca3e6d8c73bb864530d4dba5
Tomislav Simovic <tomislav.simovic@yahoo.com>
2026-03-25 10:47:03 +0100

    delete some more code for new idea

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 from interface import Interface
 
 class Models:
-    def __init__(self, model: str, category: str, costumer_request: str=None): 
-        # costumer_request gets None for an optional argument
-
+    def __init__(self, model: str, category: str): 
         self.model = model
-        # model = Personenwaagen, Rollstuhlwagen, Postwaagen etc.git
-
-        # category = Industrie, Haushalt, Medizin etc.        
         self.category = category
-
-        # add the optional part for that costumer requestgit 
-        if not costumer_request:
-            self.costumer_request = "No Entry"
-        else:
-            self.costumer_request = costumer_request
 
 
     def __str__(self):
-        return f"Model: {self.model} - Category: {self.category} - Costumer Request: {self.costumer_request}"
+        return f"Model: {self.model} - Category: {self.category}"
 
 
     def get_info(self):
